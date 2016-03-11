5d79f2b0c8f24c23af926bfd0c5c1b2be55cc9ef
DanG100 <daniel.d.j.grau@gmail.com>
2016-03-10 19:26:49 -0800

    update with Maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import gui.GUI;
 
-public class Main {
+public class Main
+{
 	public static void main(String[] args){
 		new GUI();
 	}
--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -1,3 +1,5 @@
+package gui;
+
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
 
