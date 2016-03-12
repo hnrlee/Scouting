b746be6d5c83ecb5627a006132e5d006b1583f5f
HENRY LEE <hlee4567@gmail.com>
2016-03-11 23:39:19 -0800

    fixed and added tab icons
    just resized the images instead.

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -1,7 +1,10 @@
 package gui;
 
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -16,27 +19,36 @@ public class GUI {
 	private JPanel fullMatchData = new JPanel(new GridLayout(3, 3));
 	private JPanel basicStatPanel = new JPanel(new GridLayout(3, 3));
 
-	private ImageIcon pitDataIcon = new ImageIcon("FIRST Logo.gif");
-	private ImageIcon teamMatchDataIcon = new ImageIcon("FIRST Logo.gif");
-	private ImageIcon fullMatchDataIcon = new ImageIcon("FIRST Logo.gif");
-	private ImageIcon basicStatPanelIcon = new ImageIcon("FIRST Logo.gif");
+	private ImageIcon pitDataIcon = getScaledIcon("FIRST Logo.gif");
+	private ImageIcon teamMatchDataIcon = getScaledIcon("Boulder.png");
+	private ImageIcon fullMatchDataIcon = getScaledIcon("portcullis.png");
+	private ImageIcon basicStatPanelIcon = getScaledIcon("stronghold.png");
 
 	public GUI() {
 
 		tabs.addTab("Pit Data", pitDataIcon, pitData, "Enter pit data here.");
-		tabs.setMnemonicAt(0, KeyEvent.VK_P);
+		tabs.setMnemonicAt(0, KeyEvent.VK_1);
 		tabs.addTab("Team Match Data", teamMatchDataIcon, teamMatchData, "Enter individual team match data here.");
-		tabs.setMnemonicAt(1, KeyEvent.VK_T);
+		tabs.setMnemonicAt(1, KeyEvent.VK_2);
 		tabs.addTab("Full Match Data", fullMatchDataIcon, fullMatchData, "Enter full match data here.");
-		tabs.setMnemonicAt(2, KeyEvent.VK_F);
+		tabs.setMnemonicAt(2, KeyEvent.VK_3);
 		tabs.addTab("Individual Team Statistics", basicStatPanelIcon, basicStatPanel,
 				"See data about an individual team here.");
-		tabs.setMnemonicAt(3, KeyEvent.VK_S);
+		tabs.setMnemonicAt(3, KeyEvent.VK_4);
 
 		mainFrame.add(tabs);
 		mainFrame.setSize(1000, 500);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setVisible(true);
+
+	}
+
+	public ImageIcon getScaledIcon(String filename) {
+		BufferedImage buff = new BufferedImage(new ImageIcon(filename).getImage().getWidth(null),
+				new ImageIcon(filename).getImage().getHeight(null), BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics g = buff.createGraphics();
+		g.drawImage(new ImageIcon(filename).getImage(), 0, 0, 20, 20, null);
+		return new ImageIcon(buff);
 
 	}
 
