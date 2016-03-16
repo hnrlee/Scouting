43af966bda896060f43c47b122974443977c37e2
DanG100 <daniel.d.j.grau@gmail.com>
2016-03-15 19:33:02 -0700

    it works

--- src/main/java/com/frc2367/api/WebApi.java
+++ src/main/java/com/frc2367/api/WebApi.java
@@ -1,5 +1,9 @@
 package com.frc2367.api;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,7 +17,22 @@ import javax.ws.rs.core.Response;
  */
 @Path("myresource")
 public class WebApi {
-
+	private String apiKey;
+	
+	public WebApi()
+	{
+		Scanner scan=null;
+		try
+		{
+			scan = new Scanner(new File("apiKey.txt"));
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println("error reading key");
+		}
+		apiKey = scan.nextLine();
+		System.out.println(apiKey);
+	}
     /**
      * Method handling HTTP GET requests. The returned object will be sent
      * to the client as "text/plain" media type.
@@ -25,12 +44,11 @@ public class WebApi {
     public void getTeams(String competition) 
     {
     	Client client = ClientBuilder.newClient();
-    	Response response = client.target("http://thebluealliance.com/api/v2/event/" + competition + "/teams")
-    	  .request(MediaType.TEXT_PLAIN_TYPE)
-    	  .header("X-TBA-App-Id", "frc2367:team-analysis:v0.1")
-    	  .get();
-
-    	System.out.println("status: " + response.getStatus());
+    	Response response = client.target("https://frc-api.firstinspires.org/v2.0/2016/events")
+    			  .request(MediaType.TEXT_PLAIN_TYPE)
+    			  .header("Authorization", apiKey)
+    			  .get();
+    	System.out.println("status: " + response.getStatus()); 
     	System.out.println("headers: " + response.getHeaders());
     	System.out.println("body:" + response.readEntity(String.class));
     }
