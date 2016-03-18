e78bce441bd405b314cd1bc6529ca25a9f68bcef
DanG100 <daniel.d.j.grau@gmail.com>
2016-03-17 19:49:39 -0700

    added gson, avoids excessive downloads

--- src/main/java/com/frc2367/api/WebApi.java
+++ src/main/java/com/frc2367/api/WebApi.java
@@ -17,15 +17,20 @@ import javax.ws.rs.core.Response;
  * Root resource (exposed at "myresource" path)
  */
 @Path("myresource")
-public class WebApi {
+public class WebApi
+{
 	private String apiKey;
 	private String competition;
 
-	public WebApi(String competition) {
+	public WebApi(String competition)
+	{
 		Scanner scan = null;
-		try {
+		try
+		{
 			scan = new Scanner(new File("apiKey.txt"));
-		} catch (FileNotFoundException e) {
+		}
+		catch (FileNotFoundException e)
+		{
 			System.out.println("error reading key");
 		}
 		apiKey = scan.nextLine();
@@ -36,49 +41,67 @@ public class WebApi {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 
-	public void getEvents() {
-		File f = new File("");
-		if(f.exists() && !f.isDirectory()) { 
-		    // do something
+	public void getEvents()
+	{
+		File f = new File("Cache/events.txt");
+		if (!f.exists() && !f.isDirectory())
+		{
+			Client client = ClientBuilder.newClient();
+			Response response = client.target("https://frc-api.firstinspires.org/v2.0/2016/events").request(MediaType.TEXT_PLAIN_TYPE).header("Authorization", apiKey).get();
+			System.out.println("status: " + response.getStatus());
+			System.out.println("headers: " + response.getHeaders());
+			String body = response.readEntity(String.class);
+			try
+			{
+				PrintWriter out = new PrintWriter("Cache/events.txt");
+				out.println(body);
+			}
+			catch (FileNotFoundException e)
+			{
+			}
 		}
-		Client client = ClientBuilder.newClient();
-		Response response = client.target("https://frc-api.firstinspires.org/v2.0/2016/events")
-				.request(MediaType.TEXT_PLAIN_TYPE).header("Authorization", apiKey).get();
-		System.out.println("status: " + response.getStatus());
-		System.out.println("headers: " + response.getHeaders());
-		String body = response.readEntity(String.class);
-		try {
-			PrintWriter out = new PrintWriter("Cache/events.txt");
-			out.println(body);
-		} catch (FileNotFoundException e) {
+
+	}
+
+	public void getTeams()
+	{
+		File f = new File("Cache/" + competition + "-teams.txt");
+		if (!f.exists() && !f.isDirectory())
+		{
+			Client client = ClientBuilder.newClient();
+			Response response = client.target("http://thebluealliance.com/api/v2/event/" + competition + "/teams").request(MediaType.TEXT_PLAIN_TYPE).header("X-TBA-App-Id", "frc2367:team-analysis:v0.1").get();
+
+			System.out.println("status: " + response.getStatus());
+			String body = response.readEntity(String.class);
+			try
+			{
+				PrintWriter out = new PrintWriter("Cache/" + competition + "-teams.txt");
+				out.println(body);
+			}
+			catch (FileNotFoundException e)
+			{
+			}
 		}
 	}
 
-	public void getTeams() {
-		Client client = ClientBuilder.newClient();
-		Response response = client.target("http://thebluealliance.com/api/v2/event/" + competition + "/teams")
-				.request(MediaType.TEXT_PLAIN_TYPE).header("X-TBA-App-Id", "frc2367:team-analysis:v0.1").get();
-
-		System.out.println("status: " + response.getStatus());
-		String body = response.readEntity(String.class);
-		try {
-			PrintWriter out = new PrintWriter("Cache/" + competition + "-teams.txt");
-			out.println(body);
-		} catch (FileNotFoundException e) {
-		}
-	}
-
-	public void getMatchDetails(String event, int teamNumber, String level) {
-		Client client = ClientBuilder.newClient();
-		Response response = client.target("https://frc-api.firstinspires.org/v2.0/2016/scores/"+event+"/"+ level +"?teamNumber=" + teamNumber)
-				.request(MediaType.TEXT_PLAIN_TYPE).header("Authorization", apiKey).get();
-		System.out.println("status: " + response.getStatus());
-		System.out.println("headers: " + response.getHeaders());
-		String body = response.readEntity(String.class);
-		try {
-			PrintWriter out = new PrintWriter("Cache/" + competition+ ":"+ teamNumber + ":" + level + ".txt");
-			out.println(body);
-		} catch (FileNotFoundException e) {
+	public void getMatchDetails(String event, int teamNumber, String level)
+	{
+		File f = new File("Cache/" + competition + ":" + teamNumber + ":" + level + ".txt");
+		if (!f.exists() && !f.isDirectory())
+		{
+			Client client = ClientBuilder.newClient();
+			Response response = client.target("https://frc-api.firstinspires.org/v2.0/2016/scores/" + event + "/" + level + "?teamNumber=" + teamNumber).request(MediaType.TEXT_PLAIN_TYPE).header("Authorization", apiKey).get();
+			System.out.println("status: " + response.getStatus());
+			System.out.println("headers: " + response.getHeaders());
+			String body = response.readEntity(String.class);
+			try
+			{
+				PrintWriter out = new PrintWriter("Cache/" + competition + ":" + teamNumber + ":" + level + ".txt");
+				out.println(body);
+			}
+			catch (FileNotFoundException e)
+			{
+			}
 		}
 	}
 }
