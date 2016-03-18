01dcac4b74a63481f554e99c5c2c64e995ca82c0
DanG100 <daniel.d.j.grau@gmail.com>
2016-03-17 20:56:22 -0700

    added events

---
+++ src/main/java/com/frc2367/data/events/Event.java
@@ -0,0 +1,17 @@
+package com.frc2367.data.events;
+
+public class Event
+{
+	public String code;
+	public String divisionCode;
+	public String name;
+	public String type;
+	public String districtCode;
+	public String venue;
+	public String city;
+	public String stateProv;
+	public String country;
+	public String dateStart;
+	public String dateEnd;
+	public String timezone;
+}
---
+++ src/main/java/com/frc2367/data/events/Events.java
@@ -0,0 +1,9 @@
+package com.frc2367.data.events;
+
+import java.util.ArrayList;
+
+public class Events
+{
+	public ArrayList<Event> Events = new ArrayList<Event>();
+	public int eventCount;
+}
