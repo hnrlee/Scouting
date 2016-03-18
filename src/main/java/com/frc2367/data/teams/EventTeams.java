a5108982c8d76ecee8821fbde3198809921dec94
DanG100 <daniel.d.j.grau@gmail.com>
2016-03-18 14:45:08 -0700

    added more team info classes

---
+++ src/main/java/com/frc2367/data/teams/EventTeams.java
@@ -0,0 +1,20 @@
+package com.frc2367.data.teams;
+
+import java.util.ArrayList;
+
+
+public class EventTeams
+{
+	private ArrayList<TeamInfo> teams = new ArrayList<TeamInfo>();
+
+	public ArrayList<TeamInfo> getTeams()
+	{
+		return teams;
+	}
+
+	public void setTeams(ArrayList<TeamInfo> teams)
+	{
+		this.teams = teams;
+	}
+	
+}
---
+++ src/main/java/com/frc2367/data/teams/TeamInfo.java
@@ -0,0 +1,22 @@
+package com.frc2367.data.teams;
+
+public class TeamInfo
+{
+
+	private String website;
+	private String name;
+	private String locality;
+	private int rookieYear;
+	private String region;
+	private int teamNumber;
+	private String location;
+	private String key;
+	private String countryName;
+	private String motto;
+	private String nickname;
+	
+	public TeamInfo()
+	{
+		
+	}
+}
