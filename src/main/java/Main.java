6b213873257e572665d66910251851dfcc97261e
HENRY LEE <hlee4567@gmail.com>
2016-05-09 23:40:16 -0700

    fixed jfreechart stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+import javax.swing.JFrame;
+
 import com.frc2367.api.FullTeam;
 import com.frc2367.api.WebApiTesting;
 import com.frc2367.data.ScoutedTeam;
@@ -10,14 +12,19 @@ public class Main {
 	public static void main(String[] args) {
 		GUI mainWindow = new GUI();
 		Stats stat = new Stats();
-		mainWindow.setTestScatter(stat.displayChart());
-		mainWindow.setupBasicStatPanel();
+		//mainWindow.setTestScatter(stat.displayChart());
+		//mainWindow.setupBasicStatPanel();
 		ArrayList<ScoutedTeam> teams = new ArrayList<ScoutedTeam>();
-		WebApiTesting api = new WebApiTesting(true);
+		//WebApiTesting api = new WebApiTesting(true);
 		//api.updateData();
-		ArrayList<FullTeam> arrTest = api.updateData();
+		//ArrayList<FullTeam> arrTest = api.updateData();
 		//System.out.println(api.updateData().get(10).getTeamNumber());
-		System.out.println("asdf");
+		//System.out.println("asdf");
+		JFrame j = new JFrame("asdf");
+		j.setSize(1900, 1000);
+		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		j.add(stat.displayChart());
+		j.setVisible(true);
 
 	}
 }
--- src/main/java/com/frc2367/stats/Stats.java
+++ src/main/java/com/frc2367/stats/Stats.java
@@ -2,6 +2,7 @@ package com.frc2367.stats;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -18,11 +19,17 @@ import org.jfree.data.xy.DefaultXYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
+import com.frc2367.api.FullTeam;
+import com.frc2367.api.WebApiTesting;
+
 public class Stats {
 	private DefaultXYDataset test = new DefaultXYDataset();
-	private XYSeries series = new XYSeries("2367");
 	private XYSeriesCollection seriesCollection = new XYSeriesCollection();
 	private JFreeChart scatter;
+	private WebApiTesting api = new WebApiTesting(true);
+	private ArrayList<FullTeam> arr = api.updateData();
+	private XYSeries[] bigTest = new XYSeries[arr.size()];
+	private int xAxisData;
 
 	public Stats() {
 
@@ -33,55 +40,66 @@ public class Stats {
 	}
 
 	public void createSeries() {
-		series.add(2367, 39);
-		series.add(2367, 59);
-		series.add(2367, 91);
-		series.add(2367, 99);
-		series.add(2367, 44);
-		series.add(2367, 58);
-		series.add(2367, 68);
-		series.add(2367, 64);
-		seriesCollection.addSeries(series);
+		int xMax,xMin, yMax, yMin;
+
+		for (int i = 0; i < arr.size(); i++) {
+			bigTest[i] = new XYSeries(arr.get(i).getTeamNumberAsString());
+			for (int j = 0; j < arr.get(i).getTeamScores().size(); j++) {
+				try {
+					//bigTest[i].add(arr.get(i).getTeamNumber(), arr.get(i).getTeamScores().get(j).getTotalPoints());
+					
+					bigTest[i].add(arr.get(i).getTeamScores().get(j).getAutoPoints(), arr.get(i).getTeamScores().get(j).getTotalPoints());
+				} catch (Exception e) {
+					System.out.println("ayylmao");
+				}
+			}
+			seriesCollection.addSeries(bigTest[i]);
+		}
+
+		// series.add(2367, 39);
+		// series.add(2367, 59);
+		// series.add(2367, 91);
+		// series.add(2367, 99);
+		// series.add(2367, 44);
+		// series.add(2367, 58);
+		// series.add(2367, 68);
+		// series.add(2367, 64);
+		// series.add(2367, 64);
+		// series.add(2367, 64);
+		// seriesCollection.addSeries(series);
 	}
 
 	public JPanel displayChart() {
 		JPanel panelForReturn = new JPanel();
-		
+
 		// chart setup
 		createSeries();
 		scatter = ChartFactory.createScatterPlot("Team Number vs Score", "Team Number", "Match Score", seriesCollection,
-				PlotOrientation.VERTICAL, true, true, false);
+				PlotOrientation.VERTICAL, false, false, false);
 		XYLineAndShapeRenderer render = new XYLineAndShapeRenderer();
-		render.setSeriesPaint(0, Color.RED);
-		render.setSeriesStroke(0, new BasicStroke(4.0f));
 		XYPlot plot = new XYPlot();
 
 		// configure axes
 		NumberAxis xAxis = new NumberAxis();
 		xAxis.setTickUnit(new NumberTickUnit(1000));
-		xAxis.setRange(0.0, 3000.0);
+		xAxis.setRange(0.0, 6000.0);
 		NumberAxis yAxis = new NumberAxis();
-		yAxis.setTickUnit(new NumberTickUnit(10));
-		yAxis.setRange(0.0, 200);
+		yAxis.setTickUnit(new NumberTickUnit(20));
+		yAxis.setRange(0.0, 300);
 		((XYPlot) scatter.getPlot()).setDomainAxis(xAxis);
 		((XYPlot) scatter.getPlot()).setRangeAxis(yAxis);
 
 		// rendering and displaying
 		plot.setRenderer(render);
 		panelForReturn.add(new ChartPanel(scatter));
-		JFrame frame = new JFrame();
-		frame.add(panelForReturn);
-		frame.setSize(800, 500);
-		frame.setVisible(true);
-		
-		
+
 		return panelForReturn;
 	}
 
-//	public double calcExpectedMatchPoints(Alliance a) {
-//
-//		return -1;
-//	}
+	// public double calcExpectedMatchPoints(Alliance a) {
+	//
+	// return -1;
+	// }
 
 	public double stdDevProportion() {
 		return -1;
